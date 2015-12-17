097220da2cfdfba2e7796b31591906f6b353495e
BlackKnight7 <blackknight7.h@gmail.com>
2015-12-17 19:29:08 +0800

    #8 complete the new AbsFactory.

--- AbsFactory/new/DiagramFactory.py
+++ AbsFactory/new/DiagramFactory.py
@@ -1,35 +1,34 @@
-class Diagram:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.diagram = [['*' for x in range(width)] for y in range(height)]
-
-    def add(self, component):
-        for y, row in enumerate(component.rows):
-            for x, char in enumerate(row):
-                self.diagram[y + component.y][x + component.x] = char
-
-    def save(self):
-        print(self.diagram)
-
-
-class Rectangle:
-    def __init__(self, x, y, width, height, fill, stroke):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.rows = [['%'] * width] * height
-
-
-class Text:
-    def __init__(self, x, y, text, fontsize):
-        self.x = x
-        self.y = y
-        self.rows = [list(text)]
-
-
 class DiagramFactory:
+    def __init__(self):
+        pass
+
+    class Diagram:
+        def __init__(self, width, height):
+            self.width = width
+            self.height = height
+            self.diagram = [['*' for x in range(width)] for y in range(height)]
+
+        def add(self, component):
+            for y, row in enumerate(component.rows):
+                for x, char in enumerate(row):
+                    self.diagram[y + component.y][x + component.x] = char
+
+        def save(self):
+            print(self.diagram)
+
+    class Rectangle:
+        def __init__(self, x, y, width, height, fill, stroke):
+            self.x = x
+            self.y = y
+            self.width = width
+            self.height = height
+            self.rows = [['%'] * width] * height
+
+    class Text:
+        def __init__(self, x, y, text, fontsize):
+            self.x = x
+            self.y = y
+            self.rows = [list(text)]
 
     @classmethod
     def make_diagram(Class, width, height):
--- AbsFactory/new/SvgDiagramFactory.py
+++ AbsFactory/new/SvgDiagramFactory.py
@@ -1,39 +1,42 @@
 import svgwrite
 from new.DiagramFactory import DiagramFactory
 
-class SvgDiagram:
-    def __init__(self, width, height):
-        self.height = height
-        self.width = width
-        self.dwg = svgwrite.Drawing('test.svg', profile='tiny')
-
-    def add(self, component):
-        self.dwg.add(component.rows)
-
-    def save(self):
-        self.dwg.save()
-
-class SvgRectangle:
-    def __init__(self, x, y, width, height, fill, stroke):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.rows = svgwrite.shapes.Rect(insert=(x, y), size=(width, height))
-
-class SvgText:
-    def __init__(self, x, y, text, fontsize):
-        self.x = x
-        self.y = y
-        self.rows = svgwrite.text.Text(text=text, insert=(x, y), fill='blue')
-
 
 class SvgDiagramFactory(DiagramFactory):
-    def make_diagram(self, width, height):
-        return SvgDiagram(width, height)
+    class Diagram:
+        def __init__(self, width, height):
+            self.height = height
+            self.width = width
+            self.dwg = svgwrite.Drawing('test.svg', profile='tiny')
 
-    def make_rectangle(self, x, y, width, height, fill="white", stroke="black"):
-        return SvgRectangle(x, y, width, height, fill, stroke)
+        def add(self, component):
+            self.dwg.add(component.rows)
 
-    def make_text(self, x, y, text, fontsize=12):
-        return SvgText(x, y, text, fontsize)
+        def save(self):
+            self.dwg.save()
+
+    class Rectangle:
+        def __init__(self, x, y, width, height, fill, stroke):
+            self.x = x
+            self.y = y
+            self.width = width
+            self.height = height
+            self.rows = svgwrite.shapes.Rect(insert=(x, y), size=(width, height))
+
+    class Text:
+        def __init__(self, x, y, text, fontsize):
+            self.x = x
+            self.y = y
+            self.rows = svgwrite.text.Text(text=text, insert=(x, y), fill='blue')
+
+    @classmethod
+    def make_diagram(Class, width, height):
+        return Class.Diagram(width, height)
+
+    @classmethod
+    def make_rectangle(Class, x, y, width, height, fill="white", stroke="black"):
+        return Class.Rectangle(x, y, width, height, fill, stroke)
+
+    @classmethod
+    def make_text(Class, x, y, text, fontsize=12):
+        return Class.Text(x, y, text, fontsize)
